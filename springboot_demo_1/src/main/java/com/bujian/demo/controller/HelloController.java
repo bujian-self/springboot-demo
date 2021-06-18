74720fb50ff68d925c51e6ccd1172cd22bdd55fa
lijiepersion <52983989+lijiepersion@users.noreply.github.com>
2021-06-18 12:46:56 +0800

    Add files via upload

---
+++ springboot_demo_1/src/main/java/com/bujian/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.bujian.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 第一个 测试请求
+ * @author lijie
+ * @date 2021/6/18 09:51
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "welcome to springboot demo";
+    }
+}
