4d909e31372891e1f5108d3b6323e1d3460c132c
lijiepersion <52983989+lijiepersion@users.noreply.github.com>
2021-06-18 15:55:19 +0800

    Add files via upload
    添加 单元测试

---
+++ springboot_demo_3/src/main/java/com/bujian/demo/bean/FuzideshilianDo.java
@@ -0,0 +1,34 @@
+package com.bujian.demo.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * 表数据信息
+ * @author lijie
+ * @date 2021/6/16 11:15
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName(value = "fuzideshilian")
+public class FuzideshilianDo {
+    private static final long serialVersionUID = 1L;
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String question;
+    private String answer;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime lastOperate;
+    private String status;
+    private Integer queryTimes;
+}
---
+++ springboot_demo_3/src/main/java/com/bujian/demo/service/impl/FuzideshilianServiceImpl.java
@@ -0,0 +1,31 @@
+package com.bujian.demo.service.impl;
+
+import com.bujian.demo.bean.FuzideshilianDo;
+import com.bujian.demo.mapper.FuzideshilianMapper;
+import com.bujian.demo.service.FuzideshilianService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Service层 代码的实现
+ * @author lijie
+ * @date 2021/6/16 11:15
+ */
+@Service
+public class FuzideshilianServiceImpl implements FuzideshilianService {
+
+    @Autowired
+    private FuzideshilianMapper fuzideshilianMapper;
+
+    @Override
+    public FuzideshilianDo selectById(long id) {
+        return fuzideshilianMapper.selectById(id);
+    }
+
+    @Override
+    public List<FuzideshilianDo> selectByBean(FuzideshilianDo bean) {
+        return fuzideshilianMapper.selectByBean(bean);
+    }
+}
