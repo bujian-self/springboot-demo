1420736bd430d9bebd4c9ff5fe484ae6d72352c9
lijiepersion <1084508243@qq.com>
2021-07-06 10:06:01 +0800

    更新拦截器方法 更新Swagger2展示路径

--- springboot-interceptor/HELP.md
+++ springboot-interceptor/HELP.md
@@ -2,21 +2,22 @@
 * 使用`WebMvcConfg`添加`interceptor`拦截器
 ```java
 @Configuration
-public class WebMvcConfg implements WebMvcConfigurer {
-  // 引入的2个拦截器
-  @Autowired
-  private RequestInterceptor reqInter;
-  @Autowired
-  private AuthorizeInterceptor autInter;
+public class WebMvcConfig implements WebMvcConfigurer {
+    private String prefix = "/anon";
+    // 引入的2个拦截器
+    @Autowired
+    private RequestInterceptor reqInter;
+    @Autowired
+    private AuthorizeInterceptor autInter;
 
-  /**
-   * 添加拦截器拦截的路径
-   */
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(reqInter).addPathPatterns("/**");
-    registry.addInterceptor(autInter).addPathPatterns(prefix +"/**");
-  }
+    /**
+     * 添加拦截器拦截的路径
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(reqInter).addPathPatterns("/**");
+        registry.addInterceptor(autInter).excludePathPatterns(prefix + "/**");
+    }
 }
 ```
 * `RequestInterceptor`请求拦截器
@@ -45,19 +46,150 @@ public class RequestInterceptor implements HandlerInterceptor {
 @Slf4j
 public class AuthorizeInterceptor implements HandlerInterceptor {
 
-  // 请求头文件
-  private String header = "Authorization";
+    // 请求头文件
+    private String header = "Authorization";
 
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    String auth = request.getHeader(header);
-    if (StringUtils.isBlank(auth) || !vaildAuth(auth)) {
-      response.setContentType("application/json; charset="+request.getCharacterEncoding());
-      response.getWriter().write(JSONObject.toJSONString(JSONObject.parseObject("{'time':'"+ LocalDateTime.now().toString().replace("T"," ")+"','status':'fail','message':'权限不足'}")));
-      return false;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // TODO 授权 校验
+        return true;
     }
-    return true;
-  }
+}
+```
+* 更新修改`SwaggerConfig`配置  
+1 添加 SwaggerUI资源 新的访问路径
+```
+private String prefix = "/anon";
+@Bean
+public SimpleUrlHandlerMapping swaggerUrlHandlerMapping(ServletContext servletContext, @Value("${swagger.mapping.order:10}") int order) throws Exception {
+    SimpleUrlHandlerMapping urlHandlerMapping = new SimpleUrlHandlerMapping();
+    Map<String, ResourceHttpRequestHandler> urlMap = new HashMap<>(16);
+    {
+        PathResourceResolver pathResourceResolver = new PathResourceResolver();
+        pathResourceResolver.setAllowedLocations(new ClassPathResource("META-INF/resources/webjars/"));
+        pathResourceResolver.setUrlPathHelper(new UrlPathHelper());
+
+        ResourceHttpRequestHandler resourceHttpRequestHandler = new ResourceHttpRequestHandler();
+        resourceHttpRequestHandler.setLocations(Arrays.asList(new ClassPathResource("META-INF/resources/webjars/")));
+        resourceHttpRequestHandler.setResourceResolvers(Arrays.asList(pathResourceResolver));
+        resourceHttpRequestHandler.setServletContext(servletContext);
+        resourceHttpRequestHandler.afterPropertiesSet();
+        //设置新的路径
+        urlMap.put(prefix + "/webjars/**", resourceHttpRequestHandler);
+    }
+    {
+        PathResourceResolver pathResourceResolver = new PathResourceResolver();
+        pathResourceResolver.setAllowedLocations(new ClassPathResource("META-INF/resources/"));
+        pathResourceResolver.setUrlPathHelper(new UrlPathHelper());
+
+        ResourceHttpRequestHandler resourceHttpRequestHandler = new ResourceHttpRequestHandler();
+        resourceHttpRequestHandler.setLocations(Arrays.asList(new ClassPathResource("META-INF/resources/")));
+        resourceHttpRequestHandler.setResourceResolvers(Arrays.asList(pathResourceResolver));
+        resourceHttpRequestHandler.setServletContext(servletContext);
+        resourceHttpRequestHandler.afterPropertiesSet();
+        //设置新的路径
+        urlMap.put(prefix + "/**", resourceHttpRequestHandler);
+    }
+    urlHandlerMapping.setUrlMap(urlMap);
+    //调整DispatcherServlet关于SimpleUrlHandlerMapping的排序
+    urlHandlerMapping.setOrder(order);
+    return urlHandlerMapping;
+}
+
+/**
+ * 重写 SwaggerUI 接口访问
+ */
+@Controller
+@ApiIgnore
+@RequestMapping(value = "${server.anonymous:/anon}")
+public static class SwaggerResourceController implements InitializingBean {
+
+    @Autowired
+    private ApiResourceController apiResourceController;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private DocumentationCache documentationCache;
+
+    @Autowired
+    private ServiceModelToSwagger2Mapper mapper;
+
+    @Autowired
+    private JsonSerializer jsonSerializer;
+
+    private Swagger2Controller swagger2Controller;
+
+    @Override
+    public void afterPropertiesSet() {
+        swagger2Controller = new Swagger2Controller(environment, documentationCache, mapper, jsonSerializer);
+    }
+
+    @RequestMapping("/swagger-resources/configuration/security")
+    @ResponseBody
+    public ResponseEntity<SecurityConfiguration> securityConfiguration() {
+        return apiResourceController.securityConfiguration();
+    }
+
+    @RequestMapping("/swagger-resources/configuration/ui")
+    @ResponseBody
+    public ResponseEntity<UiConfiguration> uiConfiguration() {
+        return apiResourceController.uiConfiguration();
+    }
+
+    @RequestMapping("/swagger-resources")
+    @ResponseBody
+    public ResponseEntity<List<SwaggerResource>> swaggerResources() {
+        return apiResourceController.swaggerResources();
+    }
+
+    @RequestMapping(value = "/v2/api-docs", method = RequestMethod.GET, produces = {"application/json", "application/hal+json"})
+    @ResponseBody
+    public ResponseEntity<Json> getDocumentation(
+            @RequestParam(value = "group", required = false) String swaggerGroup,
+            HttpServletRequest servletRequest) {
+        return swagger2Controller.getDocumentation(swaggerGroup, servletRequest);
+    }
+}
+```
+2 对 SwaggerUI 添加全局访问的请求头
+```
+ 在 build 后面添加 全局token
+@Bean
+public Docket docket() {
+    return new Docket(DocumentationType.SWAGGER_2)
+    .apiInfo(apiInfo())
+    .select()
+    // 指定 @RestController 注解的类
+    .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+    .paths(PathSelectors.any())
+    .build()
+    // 全局token
+    .securitySchemes(securitySchemes())
+    .securityContexts(securityContexts());
+}
+
+  设置请求头和请求的展示
+private List<SecurityContext> securityContexts() {
+    List<SecurityContext> securityContextList = new ArrayList<>();
+    List<SecurityReference> securityReferenceList = new ArrayList<>();
+    securityReferenceList.add(new SecurityReference(header, scopes()));
+    securityContextList.add(SecurityContext
+            .builder()
+            .securityReferences(securityReferenceList)
+            .forPaths(PathSelectors.any())
+            .build()
+    );
+    return securityContextList;
+}
+private AuthorizationScope[] scopes() {
+    return new AuthorizationScope[]{new AuthorizationScope("global", "accessAnything")};
+}
+private List<ApiKey> securitySchemes() {
+    List<ApiKey> apiKeyList= new ArrayList();
+    apiKeyList.add(new ApiKey(header, header, "header"));
+    return apiKeyList;
 }
 ```
 
--- springboot-interceptor/src/main/java/com/bujian/interceptor/config/SwaggerConfig.java
+++ springboot-interceptor/src/main/java/com/bujian/interceptor/config/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.bujian.interceptor.config;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -13,7 +19,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.DocumentationCache;
 import springfox.documentation.spring.web.json.Json;
 import springfox.documentation.spring.web.json.JsonSerializer;
@@ -28,6 +38,7 @@ import springfox.documentation.swagger2.web.Swagger2Controller;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 /**
  * Swagger2 构建api文档
@@ -38,6 +49,11 @@ import javax.servlet.http.HttpServletRequest;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Value("${server.interface.anonymous:/anon}")
+    private String prefix;
+    @Value("${server.interface.header:Authorization}")
+    private String header;
+
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -46,7 +62,30 @@ public class SwaggerConfig {
                 // 指定 @RestController 注解的类
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                // 全局token
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContextList = new ArrayList<>();
+        List<SecurityReference> securityReferenceList = new ArrayList<>();
+        securityReferenceList.add(new SecurityReference(header, scopes()));
+        securityContextList.add(SecurityContext
+                .builder()
+                .securityReferences(securityReferenceList)
+                .forPaths(PathSelectors.any())
+                .build()
+        );
+        return securityContextList;
+    }
+    private AuthorizationScope[] scopes() {
+        return new AuthorizationScope[]{new AuthorizationScope("global", "accessAnything")};
+    }
+    private List<ApiKey> securitySchemes() {
+        List<ApiKey> apiKeyList= new ArrayList();
+        apiKeyList.add(new ApiKey(header, header, "header"));
+        return apiKeyList;
     }
     /**
      * 构建api文档的详细信息函数
@@ -66,4 +105,108 @@ public class SwaggerConfig {
                 .description("API 描述")
                 .build();
     }
+
+
+    /**
+     * SwaggerUI资源访问
+     *
+     * @param servletContext
+     * @param order
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SimpleUrlHandlerMapping swaggerUrlHandlerMapping(ServletContext servletContext,
+                                                            @Value("${swagger.mapping.order:10}") int order) throws Exception {
+        SimpleUrlHandlerMapping urlHandlerMapping = new SimpleUrlHandlerMapping();
+        Map<String, ResourceHttpRequestHandler> urlMap = new HashMap<>(16);
+        {
+            PathResourceResolver pathResourceResolver = new PathResourceResolver();
+            pathResourceResolver.setAllowedLocations(new ClassPathResource("META-INF/resources/webjars/"));
+            pathResourceResolver.setUrlPathHelper(new UrlPathHelper());
+
+            ResourceHttpRequestHandler resourceHttpRequestHandler = new ResourceHttpRequestHandler();
+            resourceHttpRequestHandler.setLocations(Arrays.asList(new ClassPathResource("META-INF/resources/webjars/")));
+            resourceHttpRequestHandler.setResourceResolvers(Arrays.asList(pathResourceResolver));
+            resourceHttpRequestHandler.setServletContext(servletContext);
+            resourceHttpRequestHandler.afterPropertiesSet();
+            //设置新的路径
+            urlMap.put(prefix + "/webjars/**", resourceHttpRequestHandler);
+        }
+        {
+            PathResourceResolver pathResourceResolver = new PathResourceResolver();
+            pathResourceResolver.setAllowedLocations(new ClassPathResource("META-INF/resources/"));
+            pathResourceResolver.setUrlPathHelper(new UrlPathHelper());
+
+            ResourceHttpRequestHandler resourceHttpRequestHandler = new ResourceHttpRequestHandler();
+            resourceHttpRequestHandler.setLocations(Arrays.asList(new ClassPathResource("META-INF/resources/")));
+            resourceHttpRequestHandler.setResourceResolvers(Arrays.asList(pathResourceResolver));
+            resourceHttpRequestHandler.setServletContext(servletContext);
+            resourceHttpRequestHandler.afterPropertiesSet();
+            //设置新的路径
+            urlMap.put(prefix + "/**", resourceHttpRequestHandler);
+        }
+        urlHandlerMapping.setUrlMap(urlMap);
+        //调整DispatcherServlet关于SimpleUrlHandlerMapping的排序
+        urlHandlerMapping.setOrder(order);
+        return urlHandlerMapping;
+    }
+
+    /**
+     * 重写 SwaggerUI 接口访问
+     */
+    @Controller
+    @ApiIgnore
+    @RequestMapping(value = "${server.anonymous:/anon}")
+    public static class SwaggerResourceController implements InitializingBean {
+
+        @Autowired
+        private ApiResourceController apiResourceController;
+
+        @Autowired
+        private Environment environment;
+
+        @Autowired
+        private DocumentationCache documentationCache;
+
+        @Autowired
+        private ServiceModelToSwagger2Mapper mapper;
+
+        @Autowired
+        private JsonSerializer jsonSerializer;
+
+        private Swagger2Controller swagger2Controller;
+
+        @Override
+        public void afterPropertiesSet() {
+            swagger2Controller = new Swagger2Controller(environment, documentationCache, mapper, jsonSerializer);
+        }
+
+        @RequestMapping("/swagger-resources/configuration/security")
+        @ResponseBody
+        public ResponseEntity<SecurityConfiguration> securityConfiguration() {
+            return apiResourceController.securityConfiguration();
+        }
+
+        @RequestMapping("/swagger-resources/configuration/ui")
+        @ResponseBody
+        public ResponseEntity<UiConfiguration> uiConfiguration() {
+            return apiResourceController.uiConfiguration();
+        }
+
+        @RequestMapping("/swagger-resources")
+        @ResponseBody
+        public ResponseEntity<List<SwaggerResource>> swaggerResources() {
+            return apiResourceController.swaggerResources();
+        }
+
+        @RequestMapping(value = "/v2/api-docs", method = RequestMethod.GET, produces = {"application/json", "application/hal+json"})
+        @ResponseBody
+        public ResponseEntity<Json> getDocumentation(
+                @RequestParam(value = "group", required = false) String swaggerGroup,
+                HttpServletRequest servletRequest) {
+            return swagger2Controller.getDocumentation(swaggerGroup, servletRequest);
+        }
+    }
+
 }
